e62e42b45b140b7c234c1827faadf02724261fc3
Karlp-Web <karlenportillo1@gmail.com>
2025-04-02 17:28:25 +0000

    yomomma

--- Main.java
+++ Main.java
@@ -4,7 +4,17 @@ public class Main
 {
   public static void main(String[] args)
   {
+    Coffee c = new Coffee("large", true, 10, "latte");
+    SpecialityCoffee sp = new SpecialityCoffee("small", false, 30, "machiatto", "matcha");
 
+    System.out.println(c); //implicity calls c.toString();
+    System.out.println(sp); //implicity calls sp.toString();
+
+
+
+
+
+    
     // Uncomment to test the Car classes
     // CarTester.run();
 
--- SpecialtyCoffee.java
+++ SpecialtyCoffee.java
@@ -23,21 +23,23 @@ public class SpecialtyCoffee extends Coffee
   // Method override of Coffee getPrice
   public double getPrice()
   {
-    // we will edit in class
-    return 0.0;
+    // returns 1.5x the regular coffee price
+    return super.getPrice() * 1.5;
   }
 
   // Method override of Coffee toString
   public String toString()
   {
     // we will edit in class
-    return null;
+    return super.toString() + "with" + this.flavor; //cleaer super. = parents stuff, this. = my stuff
   }
 
   // print a message that compares the prices of a regular Coffee
   // and a SpecialtyCoffee using both getPrice methods
   public void printPriceComparison()
   {
-
-  }
+    System.out.println("A regular coffee costs " + super.getPrice()); //like the best
+    System.out.println("This coffee costs " + this.getPrice()); //like okay
+    System.out.println("This coffee costs " + getPrice());//like the least
+  }//use super. or this. to make sure there are no ambiguities when coding
 }
